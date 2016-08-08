46817d6f609e17b87954500c256a0d86a1075333
rylyade1 <yamdjeu.jay@gmail.com>
2016-08-08 16:58:46 +0200

    log out for CAS

--- src/main/webapp/scripts/components/auth/provider/auth.session.service.js
+++ src/main/webapp/scripts/components/auth/provider/auth.session.service.js
@@ -22,7 +22,7 @@ angular.module('sdlctoolApp')
                     // to get a new csrf token call the api                    
                     $http.get('api/account');
                     if($rootScope.ANTHENTICATIONTYPE) {
-                    	location = encodeURIComponent($rootScope.CASLOGOUTURL);
+                    	location.href = $rootScope.CASLOGOUTURL;
                     }
 
                     
