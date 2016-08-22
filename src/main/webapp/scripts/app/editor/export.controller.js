6af800008a0bcaedbb07db94c663962d0e9e8b55
rylyade1 <yamdjeu.jay@gmail.com>
2016-08-22 13:51:18 +0200

    jira configure fields

--- src/main/webapp/scripts/app/editor/export.controller.js
+++ src/main/webapp/scripts/app/editor/export.controller.js
@@ -621,21 +621,22 @@ angular.module('sdlctoolApp')
 											break;
 										 } else if($scope.jiraAlternatives.mandatoryFields[i].type === "array" && !$scope.jiraAlternatives.mandatoryFields[i].values) {
 											 // properly sets the data Structure for fields os schema type array in the scope.fields object.
-											 var tempValue = $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key].split(',');
-											 $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key] = [];
-											 if(tempValue.length > 0) {
-												 if($scope.jiraAlternatives.mandatoryFields[i].itemType === "string")
-													 $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key] = $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key].concat(tempValue);
-												 else {
-													 for(var j = 0; j < tempValue.length; j++) {
-														 if($scope.jiraAlternatives.mandatoryFields[i].itemType === "user") {
-															 $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key].push({
-																 name: tempValue[j].trim()
-															 });
-														 }
-													 }
-												 }
-											 }
+//											 var tempValue = $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key].split(',');
+//											 $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key] = [];
+//											 if(tempValue.length > 0) {
+//												 if($scope.jiraAlternatives.mandatoryFields[i].itemType === "string")
+//													 $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key] = $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key].concat(tempValue);
+//												 else {
+//													 for(var j = 0; j < tempValue.length; j++) {
+//														 if($scope.jiraAlternatives.mandatoryFields[i].itemType === "user") {
+//															 $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key].push({
+//																 name: tempValue[j].trim()
+//															 });
+//														 }
+//													 }
+//												 }
+//											 }
+											 console.log($scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key]);
 										 } else if($scope.jiraAlternatives.mandatoryFields[i].type === 'datetime') { 
 											 // creates the date format for the datetime type.
 											 $scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key] = $filter('date')($scope.fields[$scope.jiraAlternatives.mandatoryFields[i].key], "yyyy-MM-dd'T'hh:mm:ss'.000'Z");
