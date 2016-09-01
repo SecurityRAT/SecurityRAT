22e2a0b60844a7e842ad9bd402de19c84763a67a
rylyade1 <yamdjeu.jay@gmail.com>
2016-09-01 13:25:00 +0200

    test excel export safari

--- src/main/webapp/scripts/app/editor/requirements/requirements.controller.js
+++ src/main/webapp/scripts/app/editor/requirements/requirements.controller.js
@@ -1445,8 +1445,7 @@ angular.module('sdlctoolApp')
 		  var wbopts = { bookType:'xlsx', bookSST:false, type:'binary' };
 		  var wbout = XLSX.write(wb,wbopts);
 		  if(navigator.userAgent.indexOf('Safari') !== -1) {
-			  var myWindow = window.open("data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8;base64," + window.btoa(wbout));
-			  myWindow.close();
+			  window.open("data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8;base64," + window.btoa(wbout), '', 'width=300,height=150');
 		  } else {
 			  saveAs(new Blob([s2ab(wbout)], {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet ;charset=utf-8"})
 			  , appConfig.filenamePrefix + "_" + $scope.removeUnwantedChars($scope.systemSettings.name, ['/','\\', ':', '*', '?', '"', '<', '>', '|', '.']) + "_" + $scope.getCurrentDate() + ".xlsx");
