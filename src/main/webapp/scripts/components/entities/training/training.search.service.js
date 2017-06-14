d4ea25e1af916f72c0351e6b9a0beb268dc9bdf9
phzs <zsph1011@hs-karlsruhe.de>
2017-06-14 14:21:25 +0200

    Revert "Test: Adds entity training (without relations)"
    This reverts commit 808f70020034e129c019a793d21b6edff88daad5.
    
    Conflicts:
    	src/main/resources/config/liquibase/master.xml
    	src/main/webapp/index.html
    	src/main/webapp/scripts/app/trainings/training/training-detail.controller.js
    	src/main/webapp/scripts/app/trainings/training/training-detail.html
    	src/main/webapp/scripts/app/trainings/training/training-dialog.controller.js
    	src/main/webapp/scripts/app/trainings/training/training-dialog.html
    	src/main/webapp/scripts/app/trainings/training/training.controller.js
    	src/main/webapp/scripts/app/trainings/training/training.js
    	src/main/webapp/scripts/app/trainings/training/trainings.html
    	src/main/webapp/scripts/components/navbar/navbar.html

--- src/main/webapp/scripts/components/entities/training/training.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('sdlctoolApp')
-    .factory('TrainingSearch', function ($resource) {
-        return $resource('api/_search/trainings/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
