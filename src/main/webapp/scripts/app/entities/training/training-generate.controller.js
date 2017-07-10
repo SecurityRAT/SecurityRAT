180e9b96d2ad22b48af2dddcb5761ecdbb58df66
phzs <zsph1011@hs-karlsruhe.de>
2017-07-10 11:39:24 +0200

    Fixes missing dependency

--- src/main/webapp/scripts/app/entities/training/training-generate.controller.js
+++ src/main/webapp/scripts/app/entities/training/training-generate.controller.js
@@ -1,15 +1,12 @@
 angular.module('sdlctoolApp')
-    .controller('TrainingGenerateController', function ($scope, $rootScope, $stateParams, entity, Training, User) {
+    .controller('TrainingGenerateController', function ($scope, $rootScope, $stateParams, $state, entity, Training) {
         $scope.Training = entity;
-        $scope.users = User.query();
 
         var onSaveFinished = function (result) {
             $scope.$emit('sdlctoolApp:trainingUpdate', result);
         };
 
         $scope.save = function() {
-            //console.log("save() on TrainingGenerateController called");
-
             if ($scope.Training.id != null) {
                 Training.update($scope.Training, onSaveFinished);
             } else {
