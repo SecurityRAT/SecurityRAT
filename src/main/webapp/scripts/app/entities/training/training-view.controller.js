85db2d68685f7473e188d990be57bac52990b3a1
phzs <zsph1011@hs-karlsruhe.de>
2017-08-02 15:09:01 +0200

    * Updated training view controller to new loadContent usage * Changed revealjs transition to slide * Indentation fixes

--- src/main/webapp/scripts/app/entities/training/training-view.controller.js
+++ src/main/webapp/scripts/app/entities/training/training-view.controller.js
@@ -14,31 +14,33 @@ angular.module('sdlctoolApp')
                 $scope.trainingRoot.loadSubTree().then(function() {
                     console.log("TREE LOADING FINISHED", $scope.trainingRoot);
 
-                    $scope.trainingRoot.loadSlides().forEach(function(new_slide) {
-                        $scope.slides.push(new_slide);
-                    });
-                    console.log("Slides loaded:", $scope.slides);
+                    $scope.trainingRoot.loadSlides().then(function(slides) {
+                        slides.forEach(function (new_slide) {
+                            $scope.slides.push(new_slide);
+                        });
 
-                    Reveal.initialize({
-                        loop: false,
-                        controls: true,
-                        progress: true,
-                        center: true,
-                        transition: 'none', // none/fade/slide/convex/concave/zoom
 
-                        // More info https://github.com/hakimel/reveal.js#dependencies
-                        dependencies: [
-                            { src: 'bower_components/revealjs/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-                            { src: 'bower_components/revealjs/plugin/markdown/marked.js' },
-                            { src: 'bower_components/revealjs/plugin/markdown/markdown.js' },
-                            { src: 'bower_components/revealjs/plugin/notes/notes.js', async: true },
-                            { src: 'bower_components/revealjs/plugin/zoom-js/zoom.js', async: true }
-                        ]
-                    });
-                    $timeout(function() {
-                        $(document).ready(function() {
-                            $('pre code').each(function(i, block) {
-                                hljs.highlightBlock(block);
+                        Reveal.initialize({
+                            loop: false,
+                            controls: true,
+                            progress: true,
+                            center: true,
+                            transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+                            // More info https://github.com/hakimel/reveal.js#dependencies
+                            dependencies: [
+                                { src: 'bower_components/revealjs/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+                                { src: 'bower_components/revealjs/plugin/markdown/marked.js' },
+                                { src: 'bower_components/revealjs/plugin/markdown/markdown.js' },
+                                { src: 'bower_components/revealjs/plugin/notes/notes.js', async: true },
+                                { src: 'bower_components/revealjs/plugin/zoom-js/zoom.js', async: true }
+                            ]
+                        });
+                        $timeout(function() {
+                            $(document).ready(function() {
+                                $('pre code').each(function(i, block) {
+                                    hljs.highlightBlock(block);
+                                });
                             });
                         });
                     });
