6867feff6d1dea8f9b5aa2ea2fd8ee47daa34fcf
phzs <zsph1011@hs-karlsruhe.de>
2017-08-29 14:27:47 +0200

    Fix bug where training id was not resolved

--- src/main/webapp/scripts/app/entities/training/training-view.controller.js
+++ src/main/webapp/scripts/app/entities/training/training-view.controller.js
@@ -4,45 +4,52 @@ angular.module('sdlctoolApp')
                                                     apiFactory, entity, TrainingTreeNode, TrainingTreeUtil) {
         $scope.training = entity;
         $scope.slides = [];
-        TrainingTreeUtil.RootNodeOfTraining.query({id: $scope.training.id}).$promise.then(function(foundRootNode){
 
-            if(foundRootNode.id != null) {
-                TrainingTreeUtil.TreeWithPreparedContent.query({id: foundRootNode.id}).$promise.then(function (realRootNode) {
+        $scope.init = function() {
+            TrainingTreeUtil.RootNodeOfTraining.query({id: $scope.training.id}).$promise.then(function(foundRootNode){
 
-                    $scope.trainingRoot = realRootNode;
+                if(foundRootNode.id != null) {
+                    TrainingTreeUtil.TreeWithPreparedContent.query({id: foundRootNode.id}).$promise.then(function (realRootNode) {
 
-                    $scope.slides = TrainingTreeNode.getSlidesFromJson($scope.trainingRoot);
+                        $scope.trainingRoot = realRootNode;
 
-                    Reveal.initialize({
-                        loop: false,
-                        controls: true,
-                        progress: true,
-                        center: true,
-                        transition: 'slide', // none/fade/slide/convex/concave/zoom
+                        $scope.slides = TrainingTreeNode.getSlidesFromJson($scope.trainingRoot);
 
-                        // More info https://github.com/hakimel/reveal.js#dependencies
-                        dependencies: [
-                            {
-                                src: 'bower_components/revealjs/lib/js/classList.js', condition: function () {
+                        Reveal.initialize({
+                            loop: false,
+                            controls: true,
+                            progress: true,
+                            center: true,
+                            transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+                            // More info https://github.com/hakimel/reveal.js#dependencies
+                            dependencies: [
+                                {
+                                    src: 'bower_components/revealjs/lib/js/classList.js', condition: function () {
                                     return !document.body.classList;
                                 }
-                            },
-                            {src: 'bower_components/revealjs/plugin/markdown/marked.js'},
-                            {src: 'bower_components/revealjs/plugin/markdown/markdown.js'},
-                            {src: 'bower_components/revealjs/plugin/notes/notes.js', async: true},
-                            {src: 'bower_components/revealjs/plugin/zoom-js/zoom.js', async: true}
-                        ]
-                    });
-                    $timeout(function () {
-                        $(document).ready(function () {
-                            $('pre code').each(function (i, block) {
-                                hljs.highlightBlock(block);
+                                },
+                                {src: 'bower_components/revealjs/plugin/markdown/marked.js'},
+                                {src: 'bower_components/revealjs/plugin/markdown/markdown.js'},
+                                {src: 'bower_components/revealjs/plugin/notes/notes.js', async: true},
+                                {src: 'bower_components/revealjs/plugin/zoom-js/zoom.js', async: true}
+                            ]
+                        });
+                        $timeout(function () {
+                            $(document).ready(function () {
+                                $('pre code').each(function (i, block) {
+                                    hljs.highlightBlock(block);
+                                });
                             });
                         });
                     });
-                });
-            } else {
-                console.error("Could not find root node of training");
-            }
+                } else {
+                    console.error("Could not find root node of training");
+                }
+            });
+        };
+
+        $scope.training.$promise.then(function() {
+            $scope.init();
         });
     });
