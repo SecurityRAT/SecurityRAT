ca27c2944ea501fad150de14a79c888ab6f51f73
rylyade1 <rylyade@protonmail.com>
2019-05-05 09:48:19 +0200

    - Fixes issue where '/' at end of constant breaks it - Add novalidates to form

--- src/main/webapp/scripts/components/editor/jira.service.js
+++ src/main/webapp/scripts/components/editor/jira.service.js
@@ -3,24 +3,27 @@
 /* jshint undef: true */
 /* globals jsyaml, Blob, FormData */
 angular.module('sdlctoolApp')
-	.factory('JiraService', ['Helper', 'apiFactory', 'appConfig', '$q', 'SDLCToolExceptionService', function(Helper, apiFactory, appConfig, $q, SDLCToolExceptionService){
+    .factory('JiraService', ['Helper', 'apiFactory', 'appConfig', '$q', 'SDLCToolExceptionService', function (Helper, apiFactory, appConfig, $q, SDLCToolExceptionService) {
 
-		var linkTypeName = appConfig.JIRAIssueLinkTypeName;
-		var remoteRelationshipName = appConfig.JIRARemoteIssueLinkRelationshipName;
+        var linkTypeName = appConfig.JIRAIssueLinkTypeName;
+        var remoteRelationshipName = appConfig.JIRARemoteIssueLinkRelationshipName;
 
         function buildJiraUrl(value) {
-            var result = value;
+            var builtUrl = '';
             if (!value.startsWith('http')) {
-            	if (!appConfig.defaultJIRAHost.endsWith('/')) {
-                    result = appConfig.defaultJIRAHost + '/';
+                builtUrl = appConfig.defaultJIRAHost;
+                if (!appConfig.defaultJIRAHost.endsWith('/')) {
+                    builtUrl += '/';
                 }
-                result += 'browse/' + value;
+                builtUrl += 'browse/' + value;
+            } else {
+                builtUrl = value;
             }
-            
-            return result;
+
+            return builtUrl;
         }
 
-		function buildUrlCall(selector, apiUrlInfo) {
+        function buildUrlCall(selector, apiUrlInfo) {
             var baseJiraCall = apiUrlInfo.http + '//' + apiUrlInfo.host + appConfig.jiraApiPrefix + '/';
             var origin = apiUrlInfo.http + '//' + apiUrlInfo.host;
             var returnValue = '';
@@ -49,7 +52,7 @@ angular.module('sdlctoolApp')
                 case 'issueLink':
                     returnValue = origin + appConfig.jiraRestApi + '/issueLink';
                     break;
-               	case 'remotelink':
+                case 'remotelink':
                     returnValue = baseJiraCall + apiUrlInfo.ticketKey[0] + '/remotelink';
                     break;
                 case 'field':
@@ -61,9 +64,9 @@ angular.module('sdlctoolApp')
 
         function sendComment(body, ticketInfo) {
             var commentData = {
-                    'body': body
-                };
-                //adds comment to ease import
+                'body': body
+            };
+            //adds comment to ease import
             return apiFactory.postExport(buildUrlCall('comment', ticketInfo.apiUrl), commentData, { 'X-Atlassian-Token': 'nocheck', 'Content-Type': 'application/json' });
         }
 
@@ -75,7 +78,7 @@ angular.module('sdlctoolApp')
                 var data = new FormData();
                 data.append('file', blob, filename);
                 return apiFactory.postExport(buildUrlCall('attachment', ticketInfo.apiUrl), data, { 'X-Atlassian-Token': 'nocheck', 'Content-Type': undefined })
-                    .then(function(response) {
+                    .then(function (response) {
                         var commentBody = appConfig.ticketComment;
                         commentBody = commentBody.replace('§artifact_name§', fileObject.artifactName);
                         commentBody = commentBody.replace('§import_link§', appConfig.importPrefix + encodeURIComponent(response[0].self) + '.\n');
@@ -83,14 +86,14 @@ angular.module('sdlctoolApp')
 
                         //get the attachment id and save in the current requirement.
                         return sendComment(commentBody, ticketInfo);
-                    }, function(error) {
-                        if(angular.isDefined(fileObject.errorHandlingProperty))  { fileObject.errorHandlingProperty.spinnerProperty.showSpinner = false; }
+                    }, function (error) {
+                        if (angular.isDefined(fileObject.errorHandlingProperty)) { fileObject.errorHandlingProperty.spinnerProperty.showSpinner = false; }
                         if (error.status === 403) {
                             SDLCToolExceptionService.showWarning('Adding attachment unsuccessful', 'The YAML file could not be attached to the main ticket. Please check if ticket is not closed and that you have the permission to attach files.', SDLCToolExceptionService.DANGER);
                         }
                     });
             } catch (e) {
-                if(angular.isDefined(fileObject.errorHandlingProperty)) { fileObject.errorHandlingProperty.spinnerProperty.showSpinner = false ; }
+                if (angular.isDefined(fileObject.errorHandlingProperty)) { fileObject.errorHandlingProperty.spinnerProperty.showSpinner = false; }
                 SDLCToolExceptionService.showWarning('Adding attachment unsuccessful', 'YAML file could not be attached to main ticket due to file parsing error. Please contact the developers.', SDLCToolExceptionService.DANGER);
             }
         }
@@ -115,10 +118,10 @@ angular.module('sdlctoolApp')
                 'relationship': remoteRelationshipName
             };
             return apiFactory.postExport(apiCall, postData, { 'X-Atlassian-Token': 'nocheck', 'Content-Type': 'application/json' });
-                
+
         }
 
-		function addIssueLinks(mainIssueInfo, remoteIssueInfo) {
+        function addIssueLinks(mainIssueInfo, remoteIssueInfo) {
             var apiCall = buildUrlCall('issueLink', remoteIssueInfo.apiUrl);
             var promiseArray = [];
             var postData = {
@@ -140,15 +143,15 @@ angular.module('sdlctoolApp')
                 // links tickets from different JIRA instances.
                 // links ticket from main JIRA ticket to ticket in different JIRA instance
                 var inwardApiCall = buildUrlCall('remotelink', mainIssueInfo.apiUrl);
-                
+
                 promiseArray.push(createRemoteLink(inwardApiCall, remoteIssueInfo.key, remoteIssueInfo));
                 var outwardApiCall = buildUrlCall('remotelink', remoteIssueInfo.apiUrl);
                 // get the summary of the main JIRA to prepare for remote linking if necessary
                 if (angular.isUndefined(mainIssueInfo.fields)) {
-                    promiseArray.push(apiFactory.getJIRAInfo(buildUrlCall('issueKey', mainIssueInfo.apiUrl)).then(function(response) {
+                    promiseArray.push(apiFactory.getJIRAInfo(buildUrlCall('issueKey', mainIssueInfo.apiUrl)).then(function (response) {
                         mainIssueInfo.fields = response.fields;
                         return createRemoteLink(outwardApiCall, mainIssueInfo.key, mainIssueInfo);
-                        
+
                     }));
                 } else {
                     promiseArray.push(createRemoteLink(outwardApiCall, mainIssueInfo.key, mainIssueInfo));
@@ -158,49 +161,49 @@ angular.module('sdlctoolApp')
         }
 
         function removeIssueLinks(mainIssueInfo, remoteIssueInfo) {
-        	var exception = {};
-        	if(angular.equals(mainIssueInfo.apiUrl.host, remoteIssueInfo.apiUrl.host)) {
-        		var promise = $q.defer();
-        		for (var i = 0; i < mainIssueInfo.fields.issuelinks.length; i++) {
-        			if(mainIssueInfo.fields.issuelinks[i].outwardIssue.key === remoteIssueInfo.key && mainIssueInfo.fields.issuelinks[i].type.name === linkTypeName) {
-        				return apiFactory.deleteExport(buildUrlCall('issueLink', mainIssueInfo.apiUrl) + '/' + mainIssueInfo.fields.issuelinks[i].id, 
-        					{}, {'X-Atlassian-Token': 'nocheck'});
-        			}
-        		}
-        		exception.status = 404;
-        		promise.reject(exception);
-        		return promise;
-        	} else {
-        		Promise.all([apiFactory.getJIRAInfo(buildUrlCall('remotelink', mainIssueInfo.apiUrl)), apiFactory.getJIRAInfo(buildUrlCall('remotelink', remoteIssueInfo.apiUrl))])
-        		.then(function(responses) {
-        			var promiseArray = [];
-        			for (var i = 0; i < responses[0].length; i++) {
-        				if(responses[0][i].object.title === remoteIssueInfo.key && responses[0].relationship === remoteRelationshipName) {
-        					promiseArray.push(apiFactory.deleteExport(buildUrlCall('remotelink', mainIssueInfo.apiUrl) + '/' + responses[0].id, 
-        					{}, {'X-Atlassian-Token': 'nocheck'}));
+            var exception = {};
+            if (angular.equals(mainIssueInfo.apiUrl.host, remoteIssueInfo.apiUrl.host)) {
+                var promise = $q.defer();
+                for (var i = 0; i < mainIssueInfo.fields.issuelinks.length; i++) {
+                    if (mainIssueInfo.fields.issuelinks[i].outwardIssue.key === remoteIssueInfo.key && mainIssueInfo.fields.issuelinks[i].type.name === linkTypeName) {
+                        return apiFactory.deleteExport(buildUrlCall('issueLink', mainIssueInfo.apiUrl) + '/' + mainIssueInfo.fields.issuelinks[i].id,
+                            {}, { 'X-Atlassian-Token': 'nocheck' });
+                    }
+                }
+                exception.status = 404;
+                promise.reject(exception);
+                return promise;
+            } else {
+                Promise.all([apiFactory.getJIRAInfo(buildUrlCall('remotelink', mainIssueInfo.apiUrl)), apiFactory.getJIRAInfo(buildUrlCall('remotelink', remoteIssueInfo.apiUrl))])
+                    .then(function (responses) {
+                        var promiseArray = [];
+                        for (var i = 0; i < responses[0].length; i++) {
+                            if (responses[0][i].object.title === remoteIssueInfo.key && responses[0].relationship === remoteRelationshipName) {
+                                promiseArray.push(apiFactory.deleteExport(buildUrlCall('remotelink', mainIssueInfo.apiUrl) + '/' + responses[0].id,
+                                    {}, { 'X-Atlassian-Token': 'nocheck' }));
 
-        				}
-        			}
-        			for (i = 0; i < responses[1].length; i++) {
-        				if(responses[1][i].object.title === mainIssueInfo.key && responses[1].relationship === remoteRelationshipName) {
-        					promiseArray.push(apiFactory.deleteExport(buildUrlCall('remotelink', remoteIssueInfo.apiUrl) + '/' + responses[1].id, 
-        					{}, {'X-Atlassian-Token': 'nocheck'}));
+                            }
+                        }
+                        for (i = 0; i < responses[1].length; i++) {
+                            if (responses[1][i].object.title === mainIssueInfo.key && responses[1].relationship === remoteRelationshipName) {
+                                promiseArray.push(apiFactory.deleteExport(buildUrlCall('remotelink', remoteIssueInfo.apiUrl) + '/' + responses[1].id,
+                                    {}, { 'X-Atlassian-Token': 'nocheck' }));
 
-        				}
-        			}
-        			return Promise.all(promiseArray);
-        		}).catch();
-        	}
+                            }
+                        }
+                        return Promise.all(promiseArray);
+                    }).catch();
+            }
 
         }
 
-		var jiraService = {
-			buildUrlCall : buildUrlCall,
-			addAttachmentAndComment : addAttachmentAndComment,
-			addIssueLinks : addIssueLinks,
-			removeIssueLinks: removeIssueLinks,
-			buildJiraUrl: buildJiraUrl
-		};
+        var jiraService = {
+            buildUrlCall: buildUrlCall,
+            addAttachmentAndComment: addAttachmentAndComment,
+            addIssueLinks: addIssueLinks,
+            removeIssueLinks: removeIssueLinks,
+            buildJiraUrl: buildJiraUrl
+        };
 
-		return jiraService;
-	}]);
+        return jiraService;
+    }]);
