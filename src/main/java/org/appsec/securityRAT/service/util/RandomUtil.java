78949ec44c38483490b6aa4201c9a186b414cb6b
rylyade1 <rylyade@protonmail.com>
2019-10-25 10:46:20 +0200

    FIX: Security updates
    - update affecting Jhipster  issue generating weak random numbers
    - updates vulnerable npm devdependencies
    - add package-lock.json file

--- src/main/java/org/appsec/securityRAT/service/util/RandomUtil.java
+++ src/main/java/org/appsec/securityRAT/service/util/RandomUtil.java
@@ -1,7 +1,8 @@
 package org.appsec.securityRAT.service.util;
 
+import org.apache.commons.lang3.RandomStringUtils;
 
-import org.apache.commons.lang.RandomStringUtils;
+import java.security.SecureRandom;
 
 /**
  * Utility class for generating random Strings.
@@ -9,41 +10,42 @@ import org.apache.commons.lang.RandomStringUtils;
 public final class RandomUtil {
 
     private static final int DEF_COUNT = 20;
-//    private static final SecureRandom rand = new SecureRandom();
 
-    private RandomUtil() {
+    private static final SecureRandom SECURE_RANDOM;
+
+    static {
+        SECURE_RANDOM = new SecureRandom();
+        SECURE_RANDOM.nextBytes(new byte[64]);
+    }
+
+    private static String generateRandomAlphanumericString() {
+        return RandomStringUtils.random(DEF_COUNT, 0, 0, true, true, null, SECURE_RANDOM);
     }
 
     /**
-     * Generates a password.
+     * Generate a password.
      *
-     * @return the generated password
+     * @return the generated password.
      */
     public static String generatePassword() {
-        return RandomStringUtils.randomAlphanumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 
     /**
-     * Generates an activation key.
+     * Generate an activation key.
      *
-     * @return the generated activation key
+     * @return the generated activation key.
      */
     public static String generateActivationKey() {
-//    	String result = "";
-//    	for (Integer elem : rand.ints(DEF_COUNT).boxed().collect(Collectors.toList())) {
-//			result += elem.toString();
-//		};
-//		return result;
-        return RandomStringUtils.randomNumeric(DEF_COUNT);
+        return generateRandomAlphanumericString();
     }
 
     /**
-    * Generates a reset key.
-    *
-    * @return the generated reset key
-    */
-   public static String generateResetKey() {
-//	   return rand.ints(DEF_COUNT).toString();
-       return RandomStringUtils.randomNumeric(DEF_COUNT);
-   }
+     * Generate a reset key.
+     *
+     * @return the generated reset key.
+     */
+    public static String generateResetKey() {
+        return generateRandomAlphanumericString();
+    }
 }
