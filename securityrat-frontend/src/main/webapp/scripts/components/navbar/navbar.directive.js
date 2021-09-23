0aadf95fe63566b60044152a9066fd9f96973045
AresSec <reuter.rene@gmail.com>
2021-09-23 23:06:53 +0200

    added window.location.href for proper url substracting last element

--- securityrat-frontend/src/main/webapp/scripts/components/navbar/navbar.directive.js
+++ securityrat-frontend/src/main/webapp/scripts/components/navbar/navbar.directive.js
@@ -6,8 +6,7 @@ angular.module('sdlctoolApp')
             restrict: 'A',
             link: function (scope, element, attrs) {
                 var clazz = attrs.activeLink;
-                var path = attrs.href;
-                path = path.substring(1); //hack because path does bot return including hashbang
+                var path = window.location.href.replace(0,  window.location.href.lastIndexOf("/")+1);
                 scope.location = location;
                 scope.$watch('location.path()', function(newPath) {
                     if (path === newPath) {
